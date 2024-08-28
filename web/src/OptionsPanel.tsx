091556d4a3a0452f74a72009c5ed652fac7bb07e
BCiwinski <114406887+BCiwinski@users.noreply.github.com>
2024-08-29 00:56:43 +0200

    create OptionsPanel and SpaceCanvas components and style them

---
+++ web/src/OptionsPanel.tsx
@@ -0,0 +1,11 @@
+import './OptionsPanel.css'
+
+function OptionsPanel() {
+    return (
+        <div className="optionsPanel">
+            <p>Options</p>
+        </div>
+  );
+}
+
+export default OptionsPanel;
---
+++ web/src/SpaceCanvas.tsx
@@ -0,0 +1,9 @@
+import './SpaceCanvas.css'
+
+function SpaceCanvas() {
+  return (
+    <canvas className="space" width="1024" height="1024"></canvas>
+  );
+}
+
+export default SpaceCanvas;
