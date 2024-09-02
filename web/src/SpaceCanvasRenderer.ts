12c2b9c21a403d7d942ae0f5e191db4cbec84d5f
BCiwinski <114406887+BCiwinski@users.noreply.github.com>
2024-09-02 02:37:30 +0200

    implement simulation

--- web/src/SpaceCanvasRenderer.ts
+++ web/src/SpaceCanvasRenderer.ts
@@ -8,11 +8,17 @@ export default class SpaceCanvasRenderer {
 
     #width: number;
 
-    #imageData: ImageData
+    #imageData: ImageData;
 
-    #data: Uint8ClampedArray
+    #data: Uint8ClampedArray;
 
-    #stars: Array<Star> = new Array<Star>
+    #stars: Array<Star> = new Array<Star>();
+
+    gravitationScale: number = 1000.0;
+
+    #gravitationMax: number = 0.1;
+
+    #intervalId: number;
 
     constructor(context: CanvasRenderingContext2D){
 
@@ -23,10 +29,12 @@ export default class SpaceCanvasRenderer {
         this.#imageData = new ImageData(this.#width, this.#height);
         this.#data = this.#imageData.data;
 
-        this.#stars.push(new Star(100, 100))
-        this.#stars.push(new Star(150, 350))
+        this.#stars.push(new Star(400, 100))
+        this.#stars.push(new Star(550, 350))
 
         this.#render();
+
+        this.#intervalId = setInterval(() => this.#simulate(), 50);
     }
 
     createStarAt(x: number, y: number): void {
@@ -35,7 +43,48 @@ export default class SpaceCanvasRenderer {
         this.#render();
     }
 
+    #simulate() : void {
+
+        let old: Array<Star> = Array.from(this.#stars);
+
+        for (let i = 0; i < this.#stars.length; i++) {
+
+            for (let j = 0; j < old.length; j++) {
+
+                if (i == j) {
+
+                    continue;
+                }
+
+                let distanceSquared =
+                    (old[j].x - this.#stars[i].x) * (old[j].x - this.#stars[i].x)
+                    + (old[j].y - this.#stars[i].y) * (old[j].y - this.#stars[i].y);
+
+                let distance = Math.sqrt(distanceSquared);
+
+                let directionX = (old[j].x - this.#stars[i].x) / distance;
+                let directionY = (old[j].y - this.#stars[i].y) / distance;
+
+                let dx = directionX * Math.min(this.gravitationScale / distanceSquared, this.#gravitationMax);
+                let dy = directionY * Math.min(this.gravitationScale / distanceSquared, this.#gravitationMax);
+
+                this.#stars[i].momentumX += dx;
+                this.#stars[i].momentumY += dy;
+            }
+        }
+
+        this.#stars.forEach(s => {
+
+            s.x += s.momentumX;
+            s.y += s.momentumY;
+        });
+
+        this.#render();
+    }
+
     #render() {
+
+        this.#setDataOpaqueBlack();
 
         this.#stars.forEach(s => {
 
@@ -52,12 +101,31 @@ export default class SpaceCanvasRenderer {
         this.#context.putImageData(this.#imageData, 0, 0);
     }
 
+    #setDataOpaqueBlack() {
+
+        for (let i = 0; i < this.#data.length; i += 4) {
+
+            this.#data[i] = 0;
+            this.#data[i + 1] = 0;
+            this.#data[i + 2] = 0;
+            this.#data[i + 2] = 1;
+        }
+    }
+
     #getPixelPosition(x: number, y: number): number {
 
         return (y * this.#width + x) * BytesPerPixel;
     }
 
     #setPixelValue(x: number, y: number, red: number, green: number, blue: number, alpha : number = 255) {
+
+        x = Math.round(x);
+        y = Math.round(y);
+
+        if (x < 0 || y < 0 || x >= this.#width || y >= this.#height) {
+
+            return;
+        }
 
         let position : number = this.#getPixelPosition(x, y);
 
@@ -73,6 +141,10 @@ class Star {
     x: number;
 
     y: number;
+
+    momentumX: number = 0;
+
+    momentumY: number = 0;
 
     constructor(x: number, y: number) {
 
